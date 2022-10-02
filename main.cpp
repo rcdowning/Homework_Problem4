265582f00959946d87aa33f040db0c2dbba8743b
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-02 20:26:40 +0000

    Problem setup

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*****************************************************************
+Description: This code inputs a birth date and returns the user's astrological sign.
+Created by: Ryan Downing
+*****************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+  // variables
+
+  // input t number of students, outer loop
+
+  // input the birthdays, inner loop
+
+  // output sign after every outer loop
+
+
+// astrology: convert month string to an int value. month 1- can be cap or aquarius. 
+
+  
+}
